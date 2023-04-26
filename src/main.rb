fc1db1a0dfeff9554c6f8046ef7ae0482a7000b0
shoya.noguchi <nogukids@gmail.com>
2023-04-26 09:00:38 +0900

    choise character gpt

--- src/main.rb
+++ src/main.rb
@@ -11,7 +11,25 @@ puts "質問:"
 puts user_question
 puts ""
 
-magi = Magi.new("楽観的", "スティーブ・ジョブズ", "大阪のおばちゃん")
+question = <<~EOS
+以下の質問に最適な3人の人格を決めてください。
+人格は半角のカンマ区切りで返すこと
+質問:
+#{user_question}
+EOS
+create_character = OpenAIClient.new
+characters_str = create_character.completions(question)
+characters = characters_str.split(/,|、/).map {|character| character.strip}
+
+puts ""
+puts "人格選定:"
+puts question
+puts ""
+puts "人格:"
+puts characters_str
+puts ""
+
+magi = Magi.new(*characters)
 answer = magi.question(user_question)
 
 puts "最終的な回答:"
