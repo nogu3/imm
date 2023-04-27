251d42519da779e0d6684ea89c872a2581158a6e
shoya.noguchi <nogukids@gmail.com>
2023-04-28 08:50:14 +0900

    show log thread id

--- src/utils/loggable.rb
+++ src/utils/loggable.rb
@@ -5,7 +5,8 @@ module Loggable
   @@logger = Logger.new(log_path)
   @@logger.formatter = proc do |severity, datetime, progname, msg|
     now = Time.at(datetime, in: "+09:00").strftime('%Y-%m-%d %H:%M:%S.%L')
-    log = "[ #{now} ][#{severity}]: #{msg}\n"
+    threadId = Thread.current.object_id
+    log = "[ #{now} ##{threadId}] #{severity} -- #{progname}: #{msg}\n"
     puts log
     log
   end
