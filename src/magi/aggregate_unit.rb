9210e22ed3ce428d4fa73e43503f696e0a174f4d
shoya.noguchi <nogukids@gmail.com>
2023-04-27 10:41:03 +0900

    tempate change fukatsu

--- src/magi/aggregate_unit.rb
+++ src/magi/aggregate_unit.rb
@@ -3,10 +3,18 @@ require_relative './magi_unit'
 class AggregateUnit < MagiUnit
   def question_content(content)
     question_content = <<~"EOS"
-    あなたはファシリテーションのプロ
-    #{@answer_tokens}文字以内で意見の集約をして具体的な解決策を提案すること
-    なお、表題はつけず集約結果のみ回答すること
+    # 命令文
+    あなたはファシリテーションのプロです
+    以下の制約条件と入力分を元に、意見を集約して具体的な解決策を提案してください。
+
+    # 制約条件
+    ・#{@answer_tokens}文字以内で回答すること
+    ・集約した結果のみを回答すること
+
+    # 入力文
     #{content}
+
+    # 出力文
     EOS
     question_content
   end
