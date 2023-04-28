7434192e4ec99ab3c0e6db0cbc85e2e5c3714b0a
shoya.noguchi <nogukids@gmail.com>
2023-04-28 09:32:17 +0900

    change model davinci to turbo

--- src/utils/openai_client.rb
+++ src/utils/openai_client.rb
@@ -16,46 +16,59 @@ class OpenAIClient
     @client = OpenAI::Client.new(access_token: api_key)
   end
 
-  def completions(question)
-    response = request(question)
+  def chat(system_setting_character, question)
+    request_param = {
+      system_setting_character: system_setting_character,
+      question: question
+    }
 
-    logger.debug(response.body)
-
-    if error?(response)
-      puts 'すみません。うまく答えられないので、開発者の助けがほしいです。'
-      puts 'stacktrace:'
-      pp response
-      return
-    end
-
+    response = request(request_param)
     get_answer(response)
   end
 
-  def request(question)
+  def request(request_param)
     response = ""
+    is_error = false
     result_time = Benchmark.realtime do
-      response = request_without_benchmark(question)
+      response, is_error = request_without_benchmark(request_param)
     end
 
-    result = "response time is #{result_time} second."
-    logger.debug(result)
+    logger.debug("response time is #{result_time} second.")
+
+    if is_error
+      failed(response)
+    end
 
     response
   end
 
-  def request_without_benchmark(question)
-    @client.completions(
+  def request_without_benchmark(request_param)
+    response = @client.chat(
       parameters: {
         model: 'gpt-3.5-turbo',
-        prompt: question,
-        max_tokens: 2028,
-        temperature: 0.5
+        messages: [
+          { role: "system", content: request_param[:system_setting_character]},
+          { role: "user", content: request_param[:question]}
+        ],
+        temperature: 1.2,
       }
     )
+
+    logger.debug("response.body:")
+    logger.debug(response.body)
+
+    return response, error?(response)
   end
 
   def get_answer(response)
-    response['choices'][0]['text'].delete("\n")
+    response['choices'][0]['message']['content'].delete("\n")
+  end
+
+  def failed(response)
+    logger.info('すみません。うまく答えられないので、開発者の助けがほしいです。')
+    logger.info('stacktrace:')
+    logger.info(response)
+    raise RuntimeError, "openai request failed."
   end
 
   def error?(response)
